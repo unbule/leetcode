3f027144750ae16fbfceaf583c3124cb2f150624
unbule <weixin1128211@163.com>
2021-06-08 18:26:24 +0800

    add 316

--- greedy/316/316.go
+++ greedy/316/316.go
@@ -18,13 +18,13 @@ func removeDuplicateLetters(s string) string {
 	//str := ""
 	//k := 1
 	liststr[s[0]-'a'] = 1
-	for i := 1 ; i < len(s); i++ {
+	for i := 1; i < len(s); i++ {
 		fmt.Println(liststr)
 		if liststr[s[i]-'a'] == 0 {
 			liststr[s[i]-'a'] = i + 1
 			continue
 		}
-		if s[i] > s[i-1] && liststr[s[i]-'a'] < liststr[s[i-1]-'a']{
+		if s[i] > s[i-1] && liststr[s[i]-'a'] < liststr[s[i-1]-'a'] {
 			liststr[s[i]-'a'] = i + 1
 		}
 	}
@@ -37,18 +37,18 @@ func removeDuplicateLetters(s string) string {
 	return string(liststres)
 }
 
-
-func removeDuplicateLetters2(s string) string{
-	time := make([]int,26)
+//单调栈
+func removeDuplicateLetters2(s string) string {
+	time := make([]int, 26)
 	var stack []byte
-	inStack := make([]bool,26)
-	for _, char := range s{
+	inStack := make([]bool, 26)
+	for _, char := range s {
 		time[char-'a']++
 	}
-	for i := range s{
+	for i := range s {
 		ch := s[i]
 		if !inStack[ch-'a'] {
-			for len(stack) > 0 && ch <stack[len(stack)-1] && time[stack[len(stack)-1]-'a'] > 0{
+			for len(stack) > 0 && ch < stack[len(stack)-1] && time[stack[len(stack)-1]-'a'] > 0 {
 				inStack[stack[len(stack)-1]-'a'] = false
 				stack = stack[:len(stack)-1]
 			}
