c37cdcf915ba2f2698e77094e8ca188d121692f6
unbule <weixin1128211@163.com>
2021-06-10 15:32:56 +0800

    don't think understand

---
+++ greedy/874/874.go
@@ -0,0 +1,10 @@
+package greedy
+
+func robotSim(commands []int, obstacles [][]int) int {
+	ore := 1
+	x := 0
+	y := 0
+	for i:=0; i<len(commands); i++{
+		if 
+	}
+}
---
+++ greedy/874/874_test.go
@@ -0,0 +1,19 @@
+package greedy
+
+import "testing"
+
+func Test_874(t *testing.T) {
+	tests := []struct {
+		name   string
+		expect int
+		real   int
+	}{
+		{"case1", 0, 0},
+		{"case1", 0, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			//if ...
+		})
+	}
+}
