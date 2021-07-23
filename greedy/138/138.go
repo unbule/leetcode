a936787b1300647a1b428a11510857a60dea3914
unbule <weixin1128211@163.com>
2021-07-23 16:35:57 +0800

    183

---
+++ greedy/138/138.go
@@ -0,0 +1,32 @@
+package main
+
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
+
+var cachedNode map[*Node]*Node
+
+func deepCopy(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+
+	if n, has := cachedNode[node]; has {
+		return n
+	}
+	newNode := &Node{Val: node.Val}
+	cachedNode[node] = newNode
+	newNode.Next = deepCopy(node.Next)
+	newNode.Random = deepCopy(node.Random)
+	return newNode
+}
+
+func copyRandomList(head *Node) *Node {
+	cachedNode = map[*Node]*Node{}
+	return deepCopy(head)
+}
+
+func main() {
+}
