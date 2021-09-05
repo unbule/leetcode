25686301b69ded441618b7ce1a45d0e45fe27bc5
unbule <weixin1128211@163.com>
2021-09-05 13:43:48 +0800

    rand

---
+++ everydate/470/470.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+}
+
+/**
+**随机数生成器
+**(randX - 1) * Y + randY ==> 可以等概率的生成[1, X * Y]范围的随机数
+**randX % X + 1 ==> 可以等概率的生成[1, X]范围的随机数
+**/
+
+func rand10() int {
+	for {
+		in := (rand7()-1)*7 + rand7()
+		if in <= 40 {
+			return in%10 + 1
+		}
+	}
+}
+
+func rand7() int {
+	return 4
+}
