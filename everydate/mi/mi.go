8ce28037a2919ea79c558ca5500df2df9217f9ca
unbule <weixin1128211@163.com>
2022-04-26 21:01:03 +0800

    honor P1

--- everydate/mi/mi.go
+++ everydate/mi/mi.go
@@ -1,38 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	n := 8
-	fmt.Println(wx(n))
-}
+	n := ""
+	nums := make([]int, 0)
+	fmt.Scan(&n)
 
-func wx(n int) int {
-	nums := make([]int, n+1)
-	for i := 1; i <= n; i++ {
-		nums[i] = i
+	arr := strings.Split(n, ",")
+
+	for i := 0; i < len(arr); i++ {
+		num, _ := strconv.Atoi(arr[i])
+		nums = append(nums, num)
 	}
-	count := 1
-	j := 1
-	ln := len(nums)
-	//fmt.Println(nums)
-	for len(nums) > 2 {
-		count++
-		j++
-		if j >= ln {
-			j = 1
-		}
-		if count == 3 {
-			fmt.Println(j)
-			copy(nums[j:], nums[j+1:])
-			nums = nums[:len(nums)-1]
-			ln = len(nums)
-			if j == ln {
-				j = 1
+	k := 0
+	prt := make([][]int, 0)
+	for {
+		res := make([]int, 0)
+		for k = 0; k < len(nums); k++ {
+			if nums[k] != 0 {
+				res = append(res, nums[k])
+				nums[k] = 0
+				break
 			}
-			count = 1
-			fmt.Println(nums)
+		}
+		if k == len(nums) {
+			break
+		}
+		n := res[0]
+		for i := k; i < len(nums); i++ {
+			if nums[i] != 0 && n > nums[i] {
+				res = append(res, nums[i])
+				n = nums[i]
+				nums[i] = 0
+			}
+		}
+		prt = append(prt, res)
+	}
+
+	fmt.Println(len(prt))
+	for i := 0; i < len(prt); i++ {
+		for j := 0; j < len(prt[i]); j++ {
+			if j+1 == len(prt[i]) {
+				fmt.Printf("%d\n", prt[i][j])
+			} else {
+				fmt.Printf("%d,", prt[i][j])
+			}
 		}
 	}
-	return nums[1]
 }
